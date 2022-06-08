49d7d6b8dbe575850b69bc92cdba06ec93bc6589
Marios Sioutis <smarios@jaist.ac.jp>
2022-06-09 08:57:22 +0900

    Demonstrate behavior with structs

--- main.c
+++ main.c
@@ -4,5 +4,14 @@
 int main(void) {
     printf("hello, world!\n");
 
-    struct point *p;
+    struct point *pp;
+    //start autocompletion by inserting pp->
+    //nothing happens
+
+    struct point p;
+    //start autocompletion by inserting p.
+    //nothing happens
+    //BUT!
+    //with p. (space)C^n and then backspace up to .
+    //makes x and y appear!
 }
